f4b6d32aa860e5e0819904d7477ad6693f9f3251
Tim Rempel <timrempel@Tims-MacBook-Pro.fritz.box>
2023-05-15 22:16:46 +0200

    v2

---
+++ v2/main.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const box = document.querySelector('.logo');
+  
+    if (box) {
+      document.addEventListener('mousemove', function(event) {
+        
+        const { clientX, clientY } = event;
+        const { left, top, width, height } = box.getBoundingClientRect();
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+  
+        const offsetX = clientX - centerX;
+        const offsetY = clientY - centerY;
+  
+        const shadowX = offsetX * 0.05;
+        const shadowY = offsetY * 0.05;
+  
+        box.style.setProperty('--shadow-x', `${shadowX}px`);
+        box.style.setProperty('--shadow-y', `${shadowY}px`);
+      });
+    }
+  });
+  
+function nav(){
+    var progressBar = document.querySelector(".prog");
+    var sidebar = document.querySelector(".sidebar");
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight-300;
+    var progress = (scrollTop / scrollHeight) * 100;
+
+
+    if(progress < 25){
+      sidebar.style.position = "absolute";
+      sidebar.style.top = "100vh";
+      progressBar.style.height = progress + "%";
+        
+    }else if(progress < 100){
+      sidebar.style.position = "fixed";
+      sidebar.style.top = "0";
+      progressBar.style.height = progress + "%";
+    }else{
+        sidebar.style.position = "absolute";
+        sidebar.style.top = "404vh"
+    }
+}
+
+function submitForm() {
+    var form = document.getElementById("form");
+    var formData = new FormData(form);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "send-email.php", true);
+  
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        // Hier können Sie mit der Antwort arbeiten
+        document.querySelector(".respons").innerHTML = xhr.responseText;
+        
+      }
+    };
+  
+    xhr.send(formData);
+  }
