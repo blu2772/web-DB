18b92f8e5e45a09846f45528abfaee4fb6edfd59
Tim Rempel <timrempel@Tims-MacBook-Pro.fritz.box>
2023-06-03 18:31:45 +0200

    update

--- 186/main.js
+++ 186/main.js
@@ -13,3 +13,30 @@ toggleBtn.onclick = function (){
   }
 
 }
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+  
+  // Überprüft, ob das Element horizontal sichtbar ist
+  var horizontalVisible = rect.left < windowWidth && rect.right > 0;
+
+  // Überprüft, ob das Element vertikal sichtbar ist (nur der obere Teil)
+  var verticalVisible = rect.top < windowHeight 
+
+  return horizontalVisible && verticalVisible;
+}
+
+function animateOnScroll() {
+  var elements = document.querySelectorAll('.animateT, .animateB, .animateR, .animateL, .animate');
+
+  elements.forEach(function(element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('visible');
+    }else element.classList.remove('visible');
+  });
+}
+
+window.addEventListener('scroll', animateOnScroll);
+window.addEventListener('DOMContentLoaded', animateOnScroll);
