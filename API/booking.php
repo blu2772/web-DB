26706c132fdea1a7896709a644b53f02ad2a9b19
Tim <tim@chipworker.de>
2023-06-10 13:47:57 +0200

    Api

---
+++ API/booking.php
@@ -0,0 +1,39 @@
+<?php
+
+// JSON-Daten aus der POST-Anfrage abrufen
+$jsonData = file_get_contents('php://input');
+$data = json_decode($jsonData, true);
+
+// Verbindung zur Datenbank herstellen (Beispiel für MySQL)
+$host = 'localhost';
+$dbname = 'Booking';
+$username = 'Try409';
+$password = 'Try!409tr+$';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    // Fehlerbehandlung bei Verbindungsfehler
+    die('Verbindungsfehler zur Datenbank: ' . $e->getMessage());
+}
+
+// Daten in die Datenbank einfügen (Beispiel für eine Tabelle mit den Spalten "name" und "email")
+$name = $data['name'];
+$startdate = $data['startdate'];
+$enddate = $data['enddate'];
+$status = $['status']
+
+try {
+    $stmt = $db->prepare("INSERT INTO deine_tabelle (name, start_date, end_date, status) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$name, $startdate, $enddate, $status]);
+
+    // Erfolgsnachricht senden
+    $response = ['success' => true, 'message' => 'Daten erfolgreich in die Datenbank eingefügt'];
+    echo json_encode($response);
+} catch(PDOException $e) {
+    // Fehlerbehandlung bei Datenbankeinfügefehler
+    $response = ['success' => false, 'message' => 'Fehler beim Einfügen der Daten in die Datenbank'];
+    echo json_encode($response);
+}
+?>
