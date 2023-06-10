fda95505b9baddebaa659d97c8c839bf2a77a582
Tim <tim@chipworker.de>
2023-06-10 16:44:24 +0200

    test

--- API/booking.php
+++ API/booking.php
@@ -28,7 +28,7 @@ $enddate = $data['enddate'];
 $status = $data['status'];
 
 try {
-    $stmt = $db->prepare("INSERT into Booking.Kalender (name, email, startdate, enddate, status) VALUES (:name, :email, :start, :end, :stat)");
+    $stmt = $db->prepare("INSERT into Booking.Kalender (name, email, startdate, enddate, status) VALUES (test, test, test, stse, test)");
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':start', $startdate);
