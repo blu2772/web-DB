730e876d6e648b29a405d1e3b454c162ee587661
Tim <tim@chipworker.de>
2023-06-10 14:07:07 +0200

    api update

--- API/booking.php
+++ API/booking.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: POST');
 // JSON-Daten aus der POST-Anfrage abrufen
 $jsonData = file_get_contents('php://input');
 $data = json_decode($jsonData, true);
